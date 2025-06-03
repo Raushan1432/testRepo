b8cd5e04f48bdebb93760e5fc92c9e377eecf76d
Raushan1432 <raushan.k@irisbusiness.com>
2025-06-03 17:23:24 +0530

    test

---
+++ testproject/src/main/java/com/theiris/testproject/Entity/Employee.java
@@ -0,0 +1,23 @@
+package com.theiris.testproject.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+
+}
